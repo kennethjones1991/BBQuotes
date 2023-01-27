f4907a6044cd67f019f2d90dfa82ee6df0ccbda2
Kenneth Jones <kennethjones1991@outlook.com>
2023-01-27 01:33:32 -0700

    End of lesson 2; build quote model

---
+++ BBQuotes/Quote.swift
@@ -0,0 +1,29 @@
+//
+//  Quote.swift
+//  BBQuotes
+//
+//  Created by Kenneth Jones on 1/27/23.
+//
+
+import Foundation
+
+struct Quote: Codable {
+    let quote: String
+    let author: String
+    let series: String
+    
+    enum QuoteKeys: String, CodingKey {
+        case quote
+        case author
+        case series
+    }
+    
+    init(from decoder: Decoder) throws {
+        var container = try decoder.unkeyedContainer()
+        let quoteContainer = try container.nestedContainer(keyedBy: QuoteKeys.self)
+        
+        quote = try quoteContainer.decode(String.self, forKey: .quote)
+        author = try quoteContainer.decode(String.self, forKey: .author)
+        series = try quoteContainer.decode(String.self, forKey: .series)
+    }
+}
