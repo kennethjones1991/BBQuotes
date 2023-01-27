43d1fee7c06e773d5797ed739bfa4bff5554f63c
Kenneth Jones <kennethjones1991@outlook.com>
2023-01-27 01:22:03 -0700

    Initial project commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Kenneth Jones on 1/27/23.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
